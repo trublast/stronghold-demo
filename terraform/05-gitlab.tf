1bdcf1c2c7ffa985969e73e26b4a5e354275fd8b
Maksim Kiselev <maksim.kiselev@flant.com>
2025-02-10 21:47:49 +0300

    gitlab jwt auth

---
+++ terraform/05-gitlab.tf
@@ -0,0 +1,101 @@
+resource "vault_jwt_auth_backend" "gitlab" {
+    description = "Demo for gitlab"
+    path = "gitlab"
+    type = "jwt"
+    oidc_discovery_url  = "https://gitlab.demo-cluster.ru"
+    bound_issuer        = "https://gitlab.demo-cluster.ru"
+}
+
+# Создадим политику для секретов с учетом project
+resource "vault_policy" "read_by_project" {
+  name = "per-project-access"
+  policy = <<EOT
+path "gitlab-secret/data/{{identity.entity.aliases.${vault_jwt_auth_backend.gitlab.accessor}.metadata.project_path}}/*" {
+  capabilities = ["read"]
+}
+EOT
+}
+
+# Создадим политику для секретов с учетом project + env
+resource "vault_policy" "read_by_project_env" {
+  name = "per-project-access-with-env"
+  policy = <<EOT
+path "gitlab-secret/data/{{identity.entity.aliases.${vault_jwt_auth_backend.gitlab.accessor}.metadata.environment}}/{{identity.entity.aliases.${vault_jwt_auth_backend.gitlab.accessor}.metadata.project_path}}/*" {
+  capabilities = ["read"]
+}
+EOT
+}
+
+# Role for projects
+
+resource "vault_jwt_auth_backend_role" "role_by_project" {
+  backend         = vault_jwt_auth_backend.gitlab.path
+  role_name       = "read-by-gilab-project"
+  token_policies  = [vault_policy.read_by_project.name]
+
+  bound_audiences = ["gilab-access-aud"]
+  claim_mappings  = {"project_path": "project_path"}
+
+  user_claim      = "project_path"
+  role_type       = "jwt"
+  token_ttl       = 300
+}
+
+# Role for project + env
+
+resource "vault_jwt_auth_backend_role" "role_by_project_env" {
+  backend         = vault_jwt_auth_backend.gitlab.path
+  role_name       = "read-by-gilab-project-env"
+  token_policies  =[vault_policy.read_by_project_env.name]
+
+  bound_audiences = ["gilab-access-aud"]
+  claim_mappings  = {"project_path": "project_path", "environment": "environment"}
+
+  user_claim      = "project_path"
+  role_type       = "jwt"
+  token_ttl       = 300
+}
+
+
+# Создадим secret engine
+resource "vault_mount" "gitlab-secret" {
+  path        = "gitlab-secret"
+  type        = "kv"
+  options     = { version = "2" }
+  description = "Secrets for gitlab"
+}
+
+
+# секрет для проекта project-1
+resource "vault_kv_secret_v2" "gitlab-secret-project-1" {
+  mount                      = vault_mount.gitlab-secret.path
+  name                       = "project-group/project-1/mysecret"
+  data_json                  = jsonencode(
+    {
+        password       = "secret-password-for-project-1"
+    }
+  )
+}
+
+# секрет для проекта project-2
+resource "vault_kv_secret_v2" "gitlab-secret-project-2" {
+  mount                      = vault_mount.gitlab-secret.path
+  name                       = "project-group/project-2/mysecret"
+  data_json                  = jsonencode(
+    {
+        password       = "secret-password-for-project-2"
+    }
+  )
+}
+
+
+# секрет для проекта project-1
+resource "vault_kv_secret_v2" "gitlab-secret-project-1-production" {
+  mount                      = vault_mount.gitlab-secret.path
+  name                       = "production/project-group/project-1/mysecret"
+  data_json                  = jsonencode(
+    {
+        password       = "secret-password-for-project-1-production"
+    }
+  )
+}
